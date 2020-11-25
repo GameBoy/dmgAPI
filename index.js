e8ae3fcc102690de5998ec8750a7db26b0e398dc
Evan Barger <epbarger@gmail.com>
2020-11-25 12:59:11 -0500

    Add rough draft discord.js api

---
+++ index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(process.env.TOKEN);
+
+const MARKETPLACE_CHANNEL_ID = '336895311081373707';
+const MARKETPLACE_JSON_PATH = `${__dirname}/json/market.json`;
+
+client.on('ready', async () => {
+  console.log(`connected as "${client.user.tag}"`);
+
+  await updateMarketplaceJson(client);
+
+  console.log('done, disconnecting')
+  client.destroy();
+})
+
+const updateMarketplaceJson = async (client) => {
+  console.log('updating market.json');
+
+  const mpChannel = await client.channels.fetch(MARKETPLACE_CHANNEL_ID);
+  const pinnedMessages = await mpChannel.messages.fetchPinned();
+  const output = pinnedMessages.map((pin) => {
+    return {
+      user: `${pin.author.username}#${pin.author.discriminator}`,
+      message: pin.content,
+      created: pin.createdTimestamp,
+      avatar_url: pin.author.avatarURL(),
+      message_id: pin.id,
+      embeds: pin.embeds.map(embed => embed.url),
+      attachments: pin.attachments.map(attachment => attachment.url),
+    };
+  });
+
+  await fs.promises.writeFile(
+    MARKETPLACE_JSON_PATH,
+    JSON.stringify(output, null, 2)
+  );
+};
